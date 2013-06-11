977d7aa5580f65300d7aaca9234cab3113057dca
Daniel Baumann <mail@daniel-baumann.ch>
2013-06-11 19:30:19 +0200

    Removing Debian reference in GPL license headers.
    Signed-off-by: Daniel Baumann <mail@daniel-baumann.ch>

--- src/version.h
+++ src/version.h
@@ -16,7 +16,7 @@
    You should have received a copy of the GNU General Public License
    along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-   On Debian systems, the complete text of the GNU General Public License
+   The complete text of the GNU General Public License
    can be found in /usr/share/common-licenses/GPL-3 file.
 */
 
