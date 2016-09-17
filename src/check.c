307966c86dfbb016f8734e49237b5e865035bf0a
Andreas Bombe <aeb@debian.org>
2016-09-18 00:11:43 +0200

    fsck: Fix reporting of length to truncate with shared clusters
    When files share clusters and one of the two files has been selected to
    truncate fsck reports which file has been selected and the length to
    which it will be truncated. All code paths erronously printed the length
    of the file that was not going to be truncated, leading to possible user
    confusion.
    
    Signed-off-by: Andreas Bombe <aeb@debian.org>

--- src/check.c
+++ src/check.c
@@ -602,22 +602,22 @@ static int check_file(DOS_FS * fs, DOS_FILE * file)
 	    if (!owner->offset) {
 		printf("  Truncating second to %llu bytes because first "
 		       "is FAT32 root dir.\n",
-		       (unsigned long long)clusters2 * fs->cluster_size);
+		       (unsigned long long)clusters * fs->cluster_size);
 		do_trunc = 2;
 	    } else if (!file->offset) {
 		printf("  Truncating first to %llu bytes because second "
 		       "is FAT32 root dir.\n",
-		       (unsigned long long)clusters * fs->cluster_size);
+		       (unsigned long long)clusters2 * fs->cluster_size);
 		do_trunc = 1;
 	    } else if (interactive)
 		printf("1) Truncate first to %llu bytes%s\n"
 		       "2) Truncate second to %llu bytes\n",
-		       (unsigned long long)clusters * fs->cluster_size,
+		       (unsigned long long)clusters2 * fs->cluster_size,
 		       restart ? " and restart" : "",
-		       (unsigned long long)clusters2 * fs->cluster_size);
+		       (unsigned long long)clusters * fs->cluster_size);
 	    else
 		printf("  Truncating second to %llu bytes.\n",
-		       (unsigned long long)clusters2 * fs->cluster_size);
+		       (unsigned long long)clusters * fs->cluster_size);
 	    if (do_trunc != 2
 		&& (do_trunc == 1
 		    || (interactive && get_key("12", "?") == '1'))) {
