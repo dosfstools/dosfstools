6ae33fba9a70e14182137bb4f5aaa1d7c75a51a1
Pali Rohár <pali.rohar@gmail.com>
2018-03-07 17:58:05 +0100

    fsck.fat: Change default DOS codepage to 850
    The reason for change is that 850 is more common on Windowes and used by
    more regions as 437. Also it contains most of the characters that are also
    available in ISO-8859-1. And 850 is also default DOS code page used by
    mtools, which is used on Linux.

--- src/charconv.h
+++ src/charconv.h
@@ -1,7 +1,7 @@
 #ifndef _CHARCONV_H
 #define _CHARCONV_H
 
-#define DEFAULT_DOS_CODEPAGE 437
+#define DEFAULT_DOS_CODEPAGE 850
 
 int set_dos_codepage(int codepage);
 int dos_char_to_printable(char **p, unsigned char c);
