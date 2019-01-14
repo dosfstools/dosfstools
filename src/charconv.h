e8e07ad4b87785bfd83bfdd79840905e4fdf9764
Tobias Stoeckmann <tobias@stoeckmann.org>
2019-01-14 22:47:40 +0100

    Renamed len to out_size.
    As pointed out by pali it makes sense to rename len to out_size
    to make explicitly sure by proper naming that this is the actually
    available memory size at "out", not the length of a string.
    
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

--- src/charconv.h
+++ src/charconv.h
@@ -5,6 +5,6 @@
 
 int set_dos_codepage(int codepage);
 int dos_char_to_printable(char **p, unsigned char c);
-int local_string_to_dos_string(char *out, char *in, unsigned int len);
+int local_string_to_dos_string(char *out, char *in, unsigned int out_size);
 
 #endif
